ae136746066c77ac145c14912567edfb8644dc06
Josep del Rio <peprio@gmail.com>
2020-12-20 14:11:40 +0000

    Undo change

--- dist/ship.js
+++ dist/ship.js
@@ -562,8 +562,8 @@ export function buildShip(factionRandomizer, p_seed, size) {
             const hwi = Math.floor(componentWidth / 2);
             const hwe = componentWidth % 2;
             const forwards = factionRandomizer.hd(0, 1, "com4 directionc0") ** 4;
-            const backwards = factionRandomizer.hd(0, 1, "com4 directionc1") ** 4 / 10;
-            const toCenter = factionRandomizer.hd(0, 1, "com4 directionc2") ** 4 / 5;
+            const backwards = 0.1 * (factionRandomizer.hd(0, 1, "com4 directionc1") ** 4);
+            const toCenter = 0.2 * (factionRandomizer.hd(0, 1, "com4 directionc2") ** 4);
             const direction = shipRandomizer.schoose([
                 forwards * (2 - cn),
                 backwards,
