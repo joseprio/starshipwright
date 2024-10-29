e7f79168df338202f9dfd2584af393f866c93078
Josep del Rio <peprio@gmail.com>
2024-10-29 21:49:27 +0000

    Add exports for outline generators

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,5 @@
 //
 export { generateShip } from "./ship";
+export { generateOutline as generateOutlineVoronoi } from "./outline/voronoi";
+export { generateOutline as generateOutlineClassic } from "./outline/classic";
+export { generateOutline as generateOutlineMicro } from "./outline/micro";
